f4d1245a50d50f3d1e0b7497011ab8a33050046d
Sushant Jadhav <71134943+sushantjadhav111@users.noreply.github.com>
2026-03-23 14:12:50 +0530

    update delete route

--- app/routes/skill_routes.py
+++ app/routes/skill_routes.py
@@ -2,7 +2,6 @@ from flask import Blueprint
 
 from app.controllers.skill_controller import (
     create_skill,
-    delete_skill,
     get_skill,
     list_skills,
     seed_dummy_skills,
@@ -16,4 +15,4 @@ skill_bp.add_url_rule("/seed-skills", view_func=seed_dummy_skills, methods=["POS
 skill_bp.add_url_rule("/skills", view_func=list_skills, methods=["GET"])
 skill_bp.add_url_rule("/skills/<int:skill_id>", view_func=get_skill, methods=["GET"])
 skill_bp.add_url_rule("/skills/<int:skill_id>", view_func=update_skill, methods=["PUT"])
-skill_bp.add_url_rule("/skills/<int:skill_id>", view_func=delete_skill, methods=["DELETE"])
+# Removed the delete endpoint
