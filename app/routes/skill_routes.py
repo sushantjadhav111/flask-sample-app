22521e922cc5673a0d3b632191977e95f793a663
Sushant Jadhav <71134943+sushantjadhav111@users.noreply.github.com>
2026-03-23 14:14:51 +0530

    updated /skills endpoint

--- app/routes/skill_routes.py
+++ app/routes/skill_routes.py
@@ -12,7 +12,7 @@ skill_bp = Blueprint("skill_bp", __name__)
 
 skill_bp.add_url_rule("/create-skill", view_func=create_skill, methods=["POST"])
 skill_bp.add_url_rule("/seed-skills", view_func=seed_dummy_skills, methods=["POST"])
-skill_bp.add_url_rule("/skills", view_func=list_skills, methods=["GET"])
+skill_bp.add_url_rule("/get-skills", view_func=list_skills, methods=["GET"])
 skill_bp.add_url_rule("/skills/<int:skill_id>", view_func=get_skill, methods=["GET"])
 skill_bp.add_url_rule("/skills/<int:skill_id>", view_func=update_skill, methods=["PUT"])
 # Removed the delete endpoint
